1682c0f5078006b099b395a977d46c88a8b8415a
ed <edsherwin.nonog@gmail.com>
2020-06-14 12:52:04 +0800

    msyqlclient connector

--- bin/mysql-grafana.sh
+++ bin/mysql-grafana.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #python --version 
-python -m dbbot.run -b mysql://grafanaReader:password@172.17.0.7/robot_results reports/output.xml
+python3 -m dbbot.run -b mysql://grafanaReader:password@172.17.0.7/robot_results reports/output.xml
